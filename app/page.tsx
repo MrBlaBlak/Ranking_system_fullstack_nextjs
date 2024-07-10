4d04313ee0610510eb1298d51fcbf8c8de258786
Michał Romak <mrblablak@gmail.com>
2024-07-10 16:40:36 +0200

    Fix: fix for vercel

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 import Navbar from './components/Navbar'
 import BackgroundImage from './components/BackgroundImage'
-import ChatBubble from "@/app/components/ChatBubble";
 
 
 
@@ -18,7 +17,6 @@ export default function Home() {
                 <nav className="relative flex left-8 sm:left-20 md:left-32 lg:left-40 xl:left-1/4 mt-4 py-10 px-4 ">
                     <div className="grid grid-cols-1 sm:grid-cols-3 gap-y-10 xl:gap-0">
                         <Navbar/>
-                        <ChatBubble/>
                     </div>
                 </nav>
             </div>
