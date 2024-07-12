27d738f011b5894e5857ebeb8d5a3593294219f5
Michał Romak <mrblablak@gmail.com>
2024-07-12 10:09:31 +0200

    Feat: added Responsiveness to backgroundImage

--- app/components/shared/BackgroundImage.tsx
+++ app/components/shared/BackgroundImage.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import PilotBackgroundPhoto from '@/public/images/background/bg1920_2.png';
+import PilotBackgroundPhotoMobile from '@/public/images/background/bg1920_mobile.png';
 type Props = {
     classProperties?: string;
 }
@@ -7,7 +8,8 @@ const BackgroundImage = ({classProperties}: Props) => {
 
     return (
         <div className="fixed top-[-50%] left-[-50%] w-[200%] h-[200%]">
-            <Image className={`absolute inset-0 m-auto min-w-[50%] min-h-[50%] ${classProperties}`} src={PilotBackgroundPhoto} alt="Pilot"/>
+            <Image className={`hidden sm:block absolute inset-0 m-auto min-w-[50%] min-h-[50%]  ${classProperties}`} src={PilotBackgroundPhoto} alt="Pilot"/>
+            <Image className={`sm:hidden absolute inset-0 m-auto min-w-[50%] min-h-[50%]  ${classProperties}`} src={PilotBackgroundPhotoMobile} alt="Pilot"/>
         </div>
 
     );
